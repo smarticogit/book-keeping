a611945266492402fa6b8860621f04c5cfacfd71
Daniel <daniel.repres@hotmail.com>
2024-09-16 20:57:40 -0300

    feat: config fastify

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  S3_BUCKET: z.string(),
+  AWS_REGION: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables:', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
